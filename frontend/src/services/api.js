4f8ed27d934cf322af5cfe12b7f0c4b876237d2c
Nandini-Reddy-05 <nandinireddy93909@gmail.com>
2026-07-24 12:55:34 +0530

    Fix frontend API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://digital-learning-management-system.onrender.com',
+    baseURL: 'https://digital-learning-management-system.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
