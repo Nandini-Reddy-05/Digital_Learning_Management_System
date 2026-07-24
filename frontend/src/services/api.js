9c24d1d599c2f627e7e0f2420238ffa0b042e0f4
Nandini-Reddy-05 <nandinireddy93909@gmail.com>
2026-07-24 11:08:33 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://digital-learning-management-system.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
